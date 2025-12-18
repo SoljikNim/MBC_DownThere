8a3df062104e31f3f5ba90bd876224b3c8976ef3
SoljikNim <dltkd1500@gmail.com>
2025-12-18 12:44:17 +0900

    테스트씬 리스토어

---
+++ Assets/Sanghyun/Player/DeathAct.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using UnityEngine;
+
+public class DeathAct : MonoBehaviour
+{
+    public Renderer sightRender;
+    public float blackOutTIme = 2f;
+    public GameObject restartInterface;
+    public float interfaceShowTimer = 2f;
+
+    void Start()
+    {
+        sightRender.material.SetFloat("_ApertureSize", 1);
+        sightRender.material.SetFloat("_FeatheringEffect", 1);
+
+        restartInterface.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetDeath()
+    {
+        StartCoroutine(DeathCor());
+    }
+
+    IEnumerator DeathCor()
+    {
+        sightRender.material.SetFloat("_ApertureSize", 1);
+        sightRender.material.SetFloat("_FeatheringEffect", 1);
+
+        float blackOutTImer = blackOutTIme;
+        while (blackOutTImer > 0) {
+            blackOutTImer -= Time.deltaTime;
+
+            sightRender.material.SetFloat("_ApertureSize", (blackOutTImer / blackOutTIme));
+            sightRender.material.SetFloat("_FeatheringEffect", (blackOutTImer / blackOutTIme));
+            yield return null;
+        }
+        sightRender.material.SetFloat("_ApertureSize", 0);
+        sightRender.material.SetFloat("_FeatheringEffect", 0);
+
+        yield return new WaitForSeconds(interfaceShowTimer);
+
+        restartInterface.SetActive(true);
+    }
+}
